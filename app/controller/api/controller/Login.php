d0c11237e290b5f48a9588d28c7848f8f9734c41
yuxiao <dyau724@163.com>
2023-07-13 17:20:31 +0800

    补全

---
+++ app/controller/api/controller/Login.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace app\controller\api\controller;
+
+
+use app\BaseController;
+
+class Login extends BaseController
+{
+    /**
+     * @return string
+     */
+    public function index()
+    {
+        return 'hello!';
+    }
+}
